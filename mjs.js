6b3b9cb1a70b63d9001698a0f258879fff71a832
tamaraalbilleh <tamara2albella@gmail.com>
2021-02-07 23:41:17 +0200

    1st

---
+++ mjs.js
@@ -0,0 +1,8 @@
+var wel = `welcome to my page`
+alert (wel);
+prompt (please  to the following question: )
+var answer = prompt("answer with yes or no", "do you know anything about comics");
+
+if (answer == "yes") {console.log("proceed"); else {console .log("please read my page and click on the link down for more information")}
+var en = `enjoy`
+alert (en)
