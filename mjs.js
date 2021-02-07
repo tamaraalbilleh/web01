b70b68818414bbc7aa5b222fcac7ba89204ea155
tamaraalbilleh <tamara2albella@gmail.com>
2021-02-08 00:06:27 +0200

    fixed js

--- mjs.js
+++ mjs.js
@@ -1,8 +1,7 @@
 var wel = `welcome to my page`
-alert (wel);
-prompt (please  to the following question: )
-var answer = prompt("answer with yes or no", "do you know anything about comics");
-
-if (answer == "yes") {console.log("proceed"); else {console .log("please read my page and click on the link down for more information")}
 var en = `enjoy`
-alert (en)
+var on = `thank you for reading my page`
+alert (wel);
+var aswer= prompt ("do you know anything about comics? yes/no")
+if aswer == "yes" {alert (en); } else {alert (on)}
+console.log (`your answer is`+ aswer )
