98c34bc7622d961d1c2ed3c9e7af8a034f08c315
tamaraalbilleh <tamara2albella@gmail.com>
2021-02-09 13:22:09 +0200

    added functions

--- js1.js
+++ js1.js
@@ -1,14 +1,30 @@
 
 //opening action
 alert ("hello!")
+
+
+// order action
 var order = prompt ("what type of donut do you want to order? pink or white ");
-while (order !== "white" && order !== "pink")
-{
+function ord () { 
     order = prompt ("what type of donut do you want to order? pink or white ");
 }
-// repete action
+
+
+// to repeate order action if not entered correctly 
+function repete () {
+    while (order !== "white" && order !== "pink")
+{
+    ord ();
+}
+}
+repete ();
+
+
+
+// to show related imgs
 var itemorder = '';
-if (order == "white") 
+function imgs (){
+    if (order == "white") 
 {
     itemorder ='<img src= "d3.png"/>';
 }
@@ -16,6 +32,11 @@ else if(order == "pink")
 {
     itemorder = '<img src= "ss.png"/>';
 }
+}
+imgs ();
+
+
+
 var numofd = prompt ("how many donuts do you want ? please write a number ");
 var result = '';
 for (var i=0; i<numofd; i++)
@@ -23,11 +44,15 @@ for (var i=0; i<numofd; i++)
    result = result + itemorder 
 }
 document.write (result);
-var con = prompt ("do you want to confirm the purchase ? yes or no ")
+
+var con = prompt ("do you want to confirm the purchase ? yes or no ");
+function conf (){
+    con = prompt ("do you want to confirm the purchase ? yes or no ");
+}
 // repete question
 while (con !== "yes" && con !== "no")
 {
-    con = prompt ("do you want to confirm the purchase ? yes or no ")
+    conf ();
 }
 var ans ='';
 if (con == "yes")
